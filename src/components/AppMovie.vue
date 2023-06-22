b65dd130ad7852eedf191d5c4bcabe5eaae81674
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-22 12:15:43 +0200

    working on language method

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -6,33 +6,49 @@
         <h4>
             {{ titleOriginal }}
         </h4>
-        <p v-if="language === 'en' ? 'addFlags()' : ''">
-            {{ language }}
-        </p>
+        <div v-if="flagsImgList.includes(language + '.svg')">
+                <img :src="getImageLanguage(language + '.svg')" alt="country flag">
+        </div>
+            <div v-else>
+                {{ language }}
+            </div>
         <p>
             {{ rank.toFixed(1) }}
         </p>
     </div>
 </template>
 <script>
-
 export default {
     name: 'AppMovie',
-    props:{
-        titleMovie : String,
-        titleOriginal : String,
-        language : String,
-        rank : Number,
+    props: {
+        titleMovie: String,
+        titleOriginal: String,
+        language: String,
+        rank: Number,
+    },
+    data() {
+        return {
+            flagsImgList:[
+                "de.svg",
+                "es.svg",
+                "fr.svg",
+                "gb.svg",
+                "it.svg",
+                "jp.svg",
+                "um.svg"
+            ]
+        }
     },
     methods: {
-        addFlags(){
-            if(language === "en" ){
-                language = `<img src="../../public/flags/gb.svg" alt="gb">`
-            }
-        }
+        getImageLanguage: function (img) {
+            return new URL(`../assets/${img}`, import.meta.url).href;
+        },  
     },
 }
 </script>
 <style lang="scss" scoped>
-
+img{
+    height: 30px;
+}
 </style>
+
